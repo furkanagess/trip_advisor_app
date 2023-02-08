bbf92b3d302b52596ad24c369c25670b69fa87bc
furkanagess <caglarrfurkann@gmail.com>
2023-02-08 14:36:28 +0300

    first commit

---
+++ lib/core/base/view/base_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+
+class BaseView<T extends Store> extends StatefulWidget {
+  final T viewModel;
+  final Function(T model) onModelReady;
+  final VoidCallback onDispose;
+
+  final Widget Function(BuildContext context, T value) onPageBuilder;
+
+  BaseView({super.key, required this.viewModel, required this.onModelReady, required this.onDispose, required this.onPageBuilder});
+
+  @override
+  State<BaseView> createState() => _BaseViewState();
+}
+
+class _BaseViewState extends State<BaseView> {
+  @override
+  void initState() {
+    super.initState();
+    if (widget.onModelReady != null) {
+      return widget.onModelReady(widget.viewModel);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (widget.onDispose != null) {
+      return widget.onDispose();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.onPageBuilder(context, widget.viewModel);
+  }
+}
