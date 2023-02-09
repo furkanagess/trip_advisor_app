4b5b26f7d710ca6539aa355220424c047040c636
furkanagess <caglarrfurkann@gmail.com>
2023-02-09 16:44:38 +0300

    update

--- lib/view/auth/login/view/login_view.dart
+++ lib/view/auth/login/view/login_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trip_advisor_app/core/base/view/base_view.dart';
-import 'package:trip_advisor_app/view/login/viewModel/login_view_model.dart';
+import 'package:trip_advisor_app/view/auth/login/viewModel/login_view_model.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
